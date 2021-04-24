7b9fb03462f2d6b9df073560d3f8201c95d95b43
JimmyWalters99 <jwalters1@babson.edu>
2021-04-24 18:05:50 -0400

    finished defining the functions we will need to compare two colleges

--- speller.py
+++
@@ -1,4 +0,0 @@
-from didyoumean3.didyoumean import did_you_mean
-did_you_mean("fotball") # -> football
-did_you_mean("football") # -> football
-did_you_mean("ich bin mude") # -> ich bin müde
--- tuition_tracker_json.py
+++ tuition_tracker_json.py
@@ -2,7 +2,9 @@ import json
 import requests
 import csv
 import urllib.request
+import difflib
 from config import APIKEY
+
 
 def get_college_info(collegename):
 
@@ -14,17 +16,31 @@ def get_college_info(collegename):
             name = line[1]
             idnumber[name] = unitid
 
-    url = "https://www.tuitiontracker.org/school.html?unitid=164580"
     api_endpoint = f"https://www.tuitiontracker.org/data/school-data-09042019/"
     response = requests.get(f"{api_endpoint}{idnumber[collegename]}.json").json()
     return response
 
+
+def private_or_public(collegename):
+    response = get_college_info(collegename)
+    schooltype = response["control"]
+    private_or_public = ""
+    if schooltype == 1:
+        private_or_public = "Public"
+    if schooltype == 2:
+        private_or_public = "Private, Non-Profit"
+    else:
+        private_or_public = "Private, For-Proft"
+    return private_or_public
+
+
 def get_location(collegename):
     response = get_college_info(collegename)
-    city = response['city']
-    state = response['state']
+    city = response["city"]
+    state = response["state"]
     location = str(city + ", " + state)
     return location
+
 
 def get_sticker_price(collegename):
     response = get_college_info(collegename)
@@ -50,33 +66,83 @@ def get_acceptance_rate(collegename):
     accept = response["enrollment"]["perc_admitted"]
     return accept
 
+
 def get_grad_rate(collegename):
     response = get_college_info(collegename)
-    grad_rate = response['yearly_data'][3]['grad_rate_bachelors_6years_total']
+    grad_rate = response["yearly_data"][3]["grad_rate_bachelors_6years_total"]
     return grad_rate
 
-def get_current_temp(collegename):
+def get_weather_info(collegename):
     lat = get_lat(collegename)
     lon = get_lon(collegename)
-    
     url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={APIKEY}"
     f = urllib.request.urlopen(url)
     response_text = f.read().decode("utf-8")
     response_data = json.loads(response_text)
+    return response_data
 
-    main_data = response_data["main"]
+def get_current_temp(collegename):
+    response = get_weather_info(collegename)
+    main_data = response["main"]
     current_temp = main_data["temp"] * 9 / 5 - 459.67
     return current_temp
 
+def get_current_weather(collegename):
+    response = get_weather_info(collegename)
+    current_weather = response['weather'][0]['main']
+    return current_weather
+
 def get_total_enroll(collegename):
     response = get_college_info(collegename)
-    total_enroll = response['enrollment']['total_enrollment']
+    total_enroll = response["enrollment"]["total_enrollment"]
     return total_enroll
 
+def get_male_ratio(collegename):
+    response = get_college_info(collegename)
+    male_enroll = response['enrollment']['total_men']
+    total_enroll = get_total_enroll(collegename)
+    return male_enroll / total_enroll
+
+def get_female_ratio(collegename):
+    response = get_college_info(collegename)
+    female_enroll = response['enrollment']['total_women']
+    total_enroll = get_total_enroll(collegename)
+    return female_enroll / total_enroll
+
+def get_website(collegename):
+    response = get_college_info(collegename)
+    website = response['website']
+    return website
+
+def name_check(collegename):
+    """Checks the name of the user inputted college and returns if it is a valid name in the list,
+    and if not, returns 5 suggesttions of valid college names which the user may have meant"""
+    valid = False
+    with open("unitid.csv", "r") as f:
+        reader = csv.reader(f, delimiter="\t")
+        namelist = []
+        for line in reader:
+            name = line[1]
+            namelist.append(name)
+    for name in namelist:
+        if name == collegename:
+            valid = True
+    if not valid:
+        suggestions = difflib.get_close_matches(collegename, namelist, 5)
+        if len(suggestions) > 0:
+            return suggestions
+        else:
+            return valid
+    else:
+        return valid
+
+
+
 def main():
-    collegename1 = 'Babson College'
+    collegename1 = "Babson College"
     collegename2 = "Pennsylvania State University-Main Campus"
-    print(get_current_temp(collegename1))
+    invalidname = ""
+    # print(get_current_temp(collegename1))
     # print(get_sticker_price(collegename))
     # print(get_acceptance_rate(collegename), "%")
     # print(get_location(collegename1))
@@ -86,6 +152,8 @@ def main():
     # print(get_location(collegename2))
     # print(get_grad_rate(collegename2))
     # print(get_total_enroll(collegename2))
+    print(get_current_weather(collegename1))
+
 
 if __name__ == "__main__":
     main()
