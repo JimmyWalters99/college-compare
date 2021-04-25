0e04cf4c2fc0bd5b12db4fb286b264ed4575d45d
JimmyWalters99 <jwalters1@babson.edu>
2021-04-25 16:44:16 -0400

    made app

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import Flask, render_template, request
+from tuition_tracker_json import stat_list
+app = Flask(__name__)
+@app.route('/', methods = ["GET", "POST"])
+def table_maker():
+    if request.method == "POST":
+        college1 = request.form["college1"]
+        info1 = stat_list(college1)
+        #stat_list is a function which returns all the outputs from the web scrape code into one list.
+        if info:
+            return render_template(
+                "table_result.html",
+                first_choice = college1
+                info = info1
+            )
+        else:
+            return render_template("GC_HTML_CODE.html", error = True)
+    return render_template("GC_HTML_CODE.html", error = None)   
+if __name__ == "__main__":
+    app.run(debug=True)
